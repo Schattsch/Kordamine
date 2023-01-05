c91aa8ea9a5c14ce274c1756de3ee22410fabb83
Schattsch <karelsch123@gmail.com>
2023-01-05 17:27:43 +0200

    comm2

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ class Person {
     constructor()  {
         this.name = 'Kertu'
     }
+
+    printName(){
+        console.log(this.name)
+    }
 }
 
 const person = new Person()
 console.log(person)
+console.printName()
