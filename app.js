dcc6410c0af110ade1c70866f8ce5b128eb755db
KAREL <117545790+Schattsch@users.noreply.github.com>
2023-01-05 17:15:46 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import person from "./person.js";
+import prsn from "./person.js"
+
+import {cleanOutput, data} from "./utils.js";
+
+import { cleanOutput as clean } from "./utils.js"
+console.log(person)
+console.log(prsn)
+cleanOutput()
+console.log(data)
+clean()
+console.log(utils.data)
---
+++ person.js
@@ -0,0 +1,6 @@
+const person = {
+    name: 'Kertu'
+}
+
+
+export default  person
