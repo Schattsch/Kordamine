2fff239cd0ec5d3f1d5ae38b900f62a3dba35ab6
Schattsch <karelsch123@gmail.com>
2023-01-05 17:50:49 +0200

    comm6

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-//const numbers = [1, 2, 3]
-//console.log(numbers)
-//const newNumbers =[...numbers, 4]
-//console.log(newNumbers)
+const numbers = [1, 2, 3]
+const [a, ,b] = numbers
+console.log(a)
+console.log(b)
 
-//const person = {
-//    name: 'Kertu'
-//}
-
-//const newPerson = {
-//    ...person,
-//    age: 25
-//}
-//console.log (person)
-//console.log(newPerson)
-
-
-const filter = (args) => {
-    return args.filter(el => el === 1)
+const person = {
+    name: 'Kertu',
+    age: 25
 }
 
-console.log(filter(args, 1, 2, 3, 1))
+const {name} = person
+const {age} = person
+
+console.log(name)
+console.log(age)
+
+const printName = ({name}) => console.log(name)
+
+printName(person)
